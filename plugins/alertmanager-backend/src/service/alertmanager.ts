9e5ef6fd309536b7a3280eb740ee71d687bbaed9
bitspire-ci <developer@bitspire.net>
2022-06-21 15:44:31 +0000

    prepare alerts data for ui

--- plugins/alertmanager-backend/src/service/alertmanager.ts
+++ plugins/alertmanager-backend/src/service/alertmanager.ts
@@ -5,6 +5,16 @@ import fetch from 'node-fetch';
 import { RouterOptions } from './router';
 import { alertmanagers } from './alerts';
 
+type Alert = {
+  cluster: string;
+  fingerprint: string;
+  alertname: string;
+  severity: string;
+  state: string;
+  runbook_url: string;
+  summary: string;
+  labels: { [key: string]: string };
+};
 export default class AlertManager {
   logger: Logger;
 
@@ -34,12 +44,22 @@ export default class AlertManager {
 
       this.logger.debug(`alerts =======> ${JSON.stringify(alerts)}`);
 
-      const result: { name: string; alerts: any[] }[] = [];
+      const result: Alert[] = [];
       for (let i = 0; i < alerts.length; i++) {
         const response = await fetch(`${alerts[i].url}${apiPath}/alerts`);
         const data = await response.json();
-
-        result.push({ name: alerts[i].name, alerts: data });
+        for (let j = 0; j < data.length; j++) {
+          result.push({
+            cluster: alerts[i].name,
+            fingerprint: data[j].fingerprint,
+            alertname: data[j].labels.alertname,
+            severity: data[j].labels.severity,
+            state: data[j].status.state,
+            runbook_url: data[j].annotations.runbook_url,
+            summary: data[j].annotations.summary,
+            labels: data[j].labels,
+          });
+        }
       }
       this.logger.debug(`result =======> ${JSON.stringify(result)}`);
       res.json(result);
@@ -49,3 +69,12 @@ export default class AlertManager {
     }
   };
 }
+
+/*
+cluster: clusterName,
+alertname: eachAlert.labels.alertname,
+severity: eachAlert.labels.severity,
+state: eachAlert.status.state,
+runbook_url: eachAlert.annotations.runbook_url,
+summary: eachAlert.annotations.summary 
+*/
