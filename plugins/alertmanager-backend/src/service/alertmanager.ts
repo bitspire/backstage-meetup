8a652f5adb8c72fef028c39853b3c0afc2e69d35
bitspire-ci <developer@bitspire.net>
2022-06-21 17:11:02 +0000

    Alertmanger Frontend data fetching and design

--- plugins/alertmanager-backend/src/service/alertmanager.ts
+++ plugins/alertmanager-backend/src/service/alertmanager.ts
@@ -1,9 +1,20 @@
 import * as express from 'express';
 import { Config } from '@backstage/config';
 import { Logger } from 'winston';
+import fetch from 'node-fetch';
 import { RouterOptions } from './router';
 import { alertmanagers } from './alerts';
 
+type Alert = {
+  cluster: string;
+  fingerprint: string;
+  alertname: string;
+  severity: string;
+  state: string;
+  runbook_url: string;
+  summary: string;
+  labels: { [key: string]: string };
+};
 export default class AlertManager {
   logger: Logger;
 
@@ -16,6 +27,54 @@ export default class AlertManager {
   }
 
   public listAlerts: express.RequestHandler = async (_, res) => {
-    res.json(alertmanagers);
+    try {
+      const alertManagerConfig = this.config.getOptionalConfig('alertManager');
+      const apiPath = alertManagerConfig?.getOptionalString('apiPath') || '';
+      const alertsConfig =
+        alertManagerConfig?.getOptionalConfigArray('alerts') || [];
+
+      if (alertsConfig.length <= 0) {
+        res.json(alertmanagers);
+        return;
+      }
+
+      const alerts = alertsConfig.map(config => {
+        return { name: config.getString('name'), url: config.getString('url') };
+      });
+
+      this.logger.debug(`alerts =======> ${JSON.stringify(alerts)}`);
+
+      const result: Alert[] = [];
+      for (let i = 0; i < alerts.length; i++) {
+        const response = await fetch(`${alerts[i].url}${apiPath}/alerts`);
+        const data = await response.json();
+        for (let j = 0; j < data.length; j++) {
+          result.push({
+            cluster: alerts[i].name,
+            fingerprint: data[j].fingerprint,
+            alertname: data[j].labels.alertname,
+            severity: data[j].labels.severity,
+            state: data[j].status.state,
+            runbook_url: data[j].annotations.runbook_url,
+            summary: data[j].annotations.summary,
+            labels: data[j].labels,
+          });
+        }
+      }
+      this.logger.debug(`result =======> ${JSON.stringify(result)}`);
+      res.json(result);
+    } catch (error) {
+      this.logger.debug('error =======> ', error);
+      res.json(alertmanagers);
+    }
   };
 }
+
+/*
+cluster: clusterName,
+alertname: eachAlert.labels.alertname,
+severity: eachAlert.labels.severity,
+state: eachAlert.status.state,
+runbook_url: eachAlert.annotations.runbook_url,
+summary: eachAlert.annotations.summary 
+*/
--- plugins/alertmanager/src/components/AlertFetchComponent/AlertFetchComponent.tsx
+++ plugins/alertmanager/src/components/AlertFetchComponent/AlertFetchComponent.tsx
@@ -5,7 +5,7 @@ import { OpenInNew } from '@material-ui/icons';
 import Alert from '@material-ui/lab/Alert';
 import useAsync from 'react-use/lib/useAsync';
 import { configApiRef, useApi } from '@backstage/core-plugin-api';
-
+import { StatusWarning, StatusAborted, StatusError } from '@backstage/core-components';
 
 const useStyles = makeStyles(theme => ({
   avatar: {
@@ -23,8 +23,13 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 type Alerts = {
-  name: string;
-  alerts: any[];
+  cluster: string,
+  alertname: string,
+  severity: string,
+  state: string,
+  runbook_url: string,
+  summary: string,
+
 }
 
 
@@ -33,71 +38,107 @@ type DenseTableProps = {
 };
 
 export const FilterTable = ({ alertManagers }: DenseTableProps) => {
-    const classes = useStyles();
-    const columns: TableColumn[] = [
-      {
-        title: 'Cluster',
-        field: 'cluster',
-        highlight: true,
-      },
-      {
-        title: 'Alertname',
-        field: 'alertname',
-        highlight: true,
-      },
-      {
-        title: 'Severity',
-        field: 'severity',
-      },
-      {
-        title: 'State',
-        field: 'state',
-       
-      },
-      {
-        title: 'Runbook-URL',
-        field: 'runbook_url',
-      },
-      {
-        title: 'Summary',
-        field: 'summary',
-      },
-    ];
-
-    const filters: TableFilter[] = [
-      {
-        column: 'Cluster',
-        type: 'select',
-      },
+  const classes = useStyles();
+  const columns: TableColumn[] = [
     {
-        column: 'Severity',
-        type: 'multiple-select',
+      title: 'Cluster',
+      field: 'cluster',
+      highlight: true,
+    },
+    {
+      title: 'Alertname',
+      field: 'alertname',
+      highlight: true,
+    },
+    {
+      title: 'Severity',
+      field: 'severity',
+    },
+    {
+      title: 'State',
+      field: 'state',
+
+    },
+    {
+      title: 'Summary',
+      field: 'summary',
+    },
+  ];
+
+  const filters: TableFilter[] = [
+    {
+      column: 'Cluster',
+      type: 'multiple-select',
+    },
+    {
+      column: 'Alertname',
+      type: 'multiple-select',
+    },
+  ];
+  const data = alertManagers.map(alert => {
+    if (alert.severity === "none") {
+      return {
+        cluster: alert.cluster,
+        alertname: alert.alertname,
+
+        severity: <StatusAborted>None</StatusAborted>,
+        state: alert.state,
+        runbook_url: alert.runbook_url,
+        summary: alert.summary
+      }
+    }
+    else if (alert.severity === "warning") {
+
+      return {
+        cluster: alert.cluster,
+        alertname: alert.alertname,
+        severity: <StatusWarning> Warning</StatusWarning>,
+        state: alert.state,
+        runbook_url: alert.runbook_url,
+        summary: alert.summary
+      }
+    }
+    else if (alert.severity === "critical") {
+      return {
+        cluster: alert.cluster,
+        alertname: alert.alertname,
+        severity: <StatusError>Critical</StatusError>,
+        state: alert.state,
+        runbook_url: alert.runbook_url,
+        summary: alert.summary
+      }
+    }
+  })
+
+
+  return (
+    <div className={classes.container}>
+      <Table
+        options={{ paging: false, padding: 'dense', actionsColumnIndex: -1 }}
+        data={data}
+        actions={actions}
+        columns={columns}
+        filters={filters}
+      />
+    </div>
+  );
+
+}
+const actions: TableProps<Alerts>['actions'] = [
+  ({ runbook_url }) => {
+    return {
+      icon: () => <OpenInNew aria-label="View" />,
+      tooltip: 'Runbook',
+      disabled: !runbook_url,
+      onClick: () => {
+        if (!runbook_url) return;
+        window.open(runbook_url, '_blank');
       },
-    ];
-    const data = alertManagers.map( alert => {
-        return {
-          cluster: alert.name,
-          alertname: alert.name,
-          severity: alert.name,
-          state: alert.name,
-          runbook_url: (<OpenInNew/>),
-          summary: alert.name
-        };
-      })
-  
+    };
+  },
 
-    return (
-          <div className={classes.container}>
-            <Table
-              options={{ paging: false, padding: 'dense' }}
-              data={data}
-              columns={columns}
-              filters={filters}
-            />
-          </div>
-        );
+];
 
-  }
 
 
 export const AlertFetchComponent = () => {
@@ -106,7 +147,7 @@ export const AlertFetchComponent = () => {
   const { value, loading, error } = useAsync(async (): Promise<Alerts[]> => {
     const response = await fetch(`${backendUrl}/api/alertmanager`);
     const data = await response.json();
-console.log(data)
+
     return data;
   }, []);
 
@@ -117,5 +158,5 @@ console.log(data)
   }
 
 
-return <FilterTable alertManagers={value || []}/>
+  return <FilterTable alertManagers={value || []} />
 };
